fb7cf5f9ccd9e961b0705f3ae5fd6216392398c2
Thiago H S Ramos <thiagoramos@gmail.com>
2019-10-15 01:40:34 -0300

    W.I.P Refact fragments on Home to components (#1)

---
+++ src/assets/jss/paginationButtonStyle.js
@@ -0,0 +1,17 @@
+export default {
+  background: "#009688",
+  border: 0,
+  borderRadius: 2,
+  "-webkit-transition": "all .3s",
+  transition: "all .3s",
+  margin: "0 3px",
+  padding: 10,
+  minWidth: 30,
+  height: 30,
+  color: "#fff",
+  fontWeight: "400",
+  fontSize: 12,
+  textTransform: "uppercase",
+  textAlign: "center",
+  cursor: "pointer"
+};
---
+++ src/components/ListPagination.vue
@@ -0,0 +1,66 @@
+<template>
+  <div v-bind:class="classes.paginationHolder">
+    <button
+      v-bind:class="pagination.first ? classes.pageItem : classes.disabledPage"
+      v-bind:disabled="!pagination.first"
+      v-on:click="getRepositories('', pagination.first)"
+    >
+      primeira
+    </button>
+    <button
+      v-bind:class="pagination.prev ? classes.pageItem : classes.disabledPage"
+      v-bind:disabled="!pagination.prev"
+      v-on:click="getRepositories('', pagination.prev)"
+    >
+      anterior
+    </button>
+    <button
+      v-bind:class="pagination.next ? classes.pageItem : classes.disabledPage"
+      v-bind:disabled="!pagination.next"
+      v-on:click="getRepositories('', pagination.next)"
+    >
+      próxima
+    </button>
+    <button
+      v-bind:class="pagination.last ? classes.pageItem : classes.disabledPage"
+      v-bind:disabled="!pagination.last"
+      v-on:click="getRepositories('', pagination.last)"
+    >
+      última
+    </button>
+  </div>
+</template>
+<script>
+import classes from "@/utils/cssTranspilation";
+import paginationButtonStyle from "@/assets/jss/paginationButtonStyle";
+
+const styles = {
+  paginationHolder: {
+    marginTop: 20
+  },
+  pageItem: {
+    ...paginationButtonStyle
+  },
+  disabledPage: {
+    ...paginationButtonStyle,
+    background: "#ececec"
+  }
+};
+
+export default {
+  name: "ListPagination",
+  data() {
+    return {
+      classes: classes(styles)
+    };
+  },
+  props: {
+    getRepositories: {
+      type: Function
+    },
+    pagination: {
+      type: Object
+    }
+  }
+};
+</script>
