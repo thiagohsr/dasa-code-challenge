8d1fc8cc0e5968db2cf3e8cc4e5c58ec640bb46b
Thiago Ramos <thiagoramos@gmail.com>
2019-10-16 17:33:46 -0300

    Add tests to Githubsearch component

--- src/services/github.js
+++ src/services/github.js
@@ -1,20 +1,20 @@
-import Vue from "vue";
+import axios from "axios";
 import { GITHUB_USER_REPOS } from "@/constants/urls";
 
 const getUserRepositories = async user =>
-  await Vue.axios
+  await axios
     .get(GITHUB_USER_REPOS.replace("{{username}}", user))
     .then(result => result)
     .catch(error => error);
 
 const getRepositories = async url =>
-  await Vue.axios
+  await axios
     .get(url)
     .then(result => result)
     .catch(error => error);
 
 const getRepositoriesFromPagination = async url =>
-  await Vue.axios
+  await axios
     .get(url)
     .then(result => result)
     .catch(error => error);
