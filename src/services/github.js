a4fece2354fc0a9b960e9efdb514f283f499e6cc
Thiago Ramos <thiagoramos@gmail.com>
2019-10-20 09:53:03 -0300

    Remove unecessary calls to api from github service

--- src/services/github.js
+++ src/services/github.js
@@ -1,11 +1,4 @@
 import axios from "axios";
-import { USER_REPOS_URL } from "@/constants/urls";
-
-const getUserRepositories = async user =>
-  await axios
-    .get(USER_REPOS_URL.replace("{{username}}", user))
-    .then(result => result)
-    .catch(error => error);
 
 const getRepositories = async url =>
   await axios
@@ -13,10 +6,4 @@ const getRepositories = async url =>
     .then(result => result)
     .catch(error => error);
 
-const getRepositoriesFromPagination = async url =>
-  await axios
-    .get(url)
-    .then(result => result)
-    .catch(error => error);
-
-export { getRepositories, getUserRepositories, getRepositoriesFromPagination };
+export { getRepositories };
